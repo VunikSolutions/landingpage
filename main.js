8ddfa872db4c485852f265109c2f4c2ecc5f3ae7
brunopittaf <caiobrunopittaf@gmail.com>
2024-07-29 12:57:55 -0300

    fix: ajustado versao mobile

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 import './style.scss';
+
+let iconMenuHamburguer = document.querySelector('#iconMenu');
+const closeBtn = document.getElementById('closeBtn');
+const menu = document.getElementById('side-menu');
+const menuOverlay = document.getElementById('menuOverlay');
+
+iconMenuHamburguer.addEventListener('click', () => {
+  menu.classList.add('open');
+  menuOverlay.classList.add('active');
+});
+
+closeBtn.addEventListener('click', function () {
+  menu.classList.remove('open');
+  menuOverlay.classList.remove('active');
+});
+
+menuOverlay.addEventListener('click', function () {
+  menu.classList.remove('open');
+  menuOverlay.classList.remove('active');
+});
